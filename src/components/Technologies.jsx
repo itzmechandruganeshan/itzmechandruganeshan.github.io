55fb46a680301b4c7dae319adbe2826cc38547ec
Chandru G <chandru@ispgweb.com>
2024-10-30 15:52:52 +0530

    Issue solved 'removed repo name frmo vite.config.js'

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -20,67 +20,67 @@ const categories = [
     {
         title: "Programming Languages",
         icons: [
-            { src: "/Chandru-Ganeshan-Portfolio/images/python.svg", alt: "Python", duration: 3 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/r.svg", alt: "R", duration: 3 }
+            { src: "/images/python.svg", alt: "Python", duration: 3 },
+            { src: "/images/r.svg", alt: "R", duration: 3 }
         ],
     },
     {
         title: "Databases",
         icons: [
-            { src: "/Chandru-Ganeshan-Portfolio/images/msql.svg", alt: "MySQL", duration: 5 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/neo4j.svg", alt: "Neo4j", duration: 5 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/postgresql.svg", alt: "PostgreSQL", duration: 5 }
+            { src: "/images/msql.svg", alt: "MySQL", duration: 5 },
+            { src: "/images/neo4j.svg", alt: "Neo4j", duration: 5 },
+            { src: "/images/postgresql.svg", alt: "PostgreSQL", duration: 5 }
         ],
     },
     {
         title: "Machine Learning & Computer Vision",
         icons: [
-            { src: "/Chandru-Ganeshan-Portfolio/images/tensorflow.svg", alt: "TensorFlow", duration: 4 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/scikit-learn.svg", alt: "Scikit-Learn", duration: 4 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/opencv.svg", alt: "OpenCV", duration: 6 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/keras.svg", alt: "Keras", duration: 6 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/streamlit.svg", alt: "Streamlit", duration: 3 },
+            { src: "/images/tensorflow.svg", alt: "TensorFlow", duration: 4 },
+            { src: "/images/scikit-learn.svg", alt: "Scikit-Learn", duration: 4 },
+            { src: "/images/opencv.svg", alt: "OpenCV", duration: 6 },
+            { src: "/images/keras.svg", alt: "Keras", duration: 6 },
+            { src: "/images/streamlit.svg", alt: "Streamlit", duration: 3 },
         ],
     },
     {
         title: "Data Analytics",
         icons: [
-            { src: "/Chandru-Ganeshan-Portfolio/images/excel.svg", alt: "Excel", duration: 2.5 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/powerbi.svg", alt: "Power BI", duration: 2.5 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/pandas.svg", alt: "Pandas", duration: 2.5 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/numpy.svg", alt: "NumPy", duration: 2.5 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/matplotlib.svg", alt: "Matplotlib", duration: 2.5 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/searborn.svg", alt: "Seaborn", duration: 2.5 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/plotly.svg", alt: "Plotly", duration: 2.5 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/scipy.svg", alt: "Scipy", duration: 2.5 },
+            { src: "/images/excel.svg", alt: "Excel", duration: 2.5 },
+            { src: "/images/powerbi.svg", alt: "Power BI", duration: 2.5 },
+            { src: "/images/pandas.svg", alt: "Pandas", duration: 2.5 },
+            { src: "/images/numpy.svg", alt: "NumPy", duration: 2.5 },
+            { src: "/images/matplotlib.svg", alt: "Matplotlib", duration: 2.5 },
+            { src: "/images/searborn.svg", alt: "Seaborn", duration: 2.5 },
+            { src: "/images/plotly.svg", alt: "Plotly", duration: 2.5 },
+            { src: "/images/scipy.svg", alt: "Scipy", duration: 2.5 },
         ],
     },
     {
         title: "DevOps",
         icons: [
-            { src: "/Chandru-Ganeshan-Portfolio/images/fastapi.svg", alt: "FastAPI", duration: 3 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/docker.svg", alt: "Docker", duration: 3 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/postman.svg", alt: "Postman", duration: 3 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/vscode.svg", alt: "VS Code", duration: 3 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/pycharm.svg", alt: "VS Code", duration: 3 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/flask.png", alt: "Flask", duration: 3 },
+            { src: "/images/fastapi.svg", alt: "FastAPI", duration: 3 },
+            { src: "/images/docker.svg", alt: "Docker", duration: 3 },
+            { src: "/images/postman.svg", alt: "Postman", duration: 3 },
+            { src: "/images/vscode.svg", alt: "VS Code", duration: 3 },
+            { src: "/images/pycharm.svg", alt: "VS Code", duration: 3 },
+            { src: "/images/flask.png", alt: "Flask", duration: 3 },
         ],
     },
     {
         title: "Generative AI",
         icons: [
-            { src: "/Chandru-Ganeshan-Portfolio/images/langchain.svg", alt: "LangChain", duration: 3 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/langgraph.svg", alt: "Langgraph", duration: 3 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/huggingface.svg", alt: "Hugging Face", duration: 2 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/autogen.svg", alt: "Autogen", duration: 3 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/crewai.svg", alt: "Crewa AI", duration: 3 }
+            { src: "/images/langchain.svg", alt: "LangChain", duration: 3 },
+            { src: "/images/langgraph.svg", alt: "Langgraph", duration: 3 },
+            { src: "/images/huggingface.svg", alt: "Hugging Face", duration: 2 },
+            { src: "/images/autogen.svg", alt: "Autogen", duration: 3 },
+            { src: "/images/crewai.svg", alt: "Crewa AI", duration: 3 }
         ],
     },
     {
         title: "Learning",
         icons: [
-            { src: "/Chandru-Ganeshan-Portfolio/images/react.svg", alt: "React", duration: 3 },
-            { src: "/Chandru-Ganeshan-Portfolio/images/js.svg", alt: "JavaScript", duration: 3 }
+            { src: "/images/react.svg", alt: "React", duration: 3 },
+            { src: "/images/js.svg", alt: "JavaScript", duration: 3 }
         ],
     },
 
